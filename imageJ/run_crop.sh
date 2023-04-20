f6bda11bdcdecc6449948d781cabf03357ddd2be
Athena Economides <eceva@ethz.ch>
2023-04-20 17:29:17 +0200

    update imagej path

--- imageJ/run_crop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eu
-
-/home/athena-admin/Desktop/Fiji.app/ImageJ-linux64 --headless -macro crop.ijm
---
+++ pre-processing/imageJ/run_crop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -eu
+
+~/Desktop/Fiji.app/ImageJ-linux64 --headless -macro crop.ijm
